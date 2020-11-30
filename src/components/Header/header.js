9d8ebd315408fd52dbb8cf38bba1cdcd7f92fa17
StijnWillemen <60694521+StijnWillemen@users.noreply.github.com>
2020-11-30 16:10:49 +0100

    Header

---
+++ src/components/Header/header.js
@@ -0,0 +1,45 @@
+import { Link,useStaticQuery,graphql } from "gatsby"
+import PropTypes from "prop-types"
+import React from "react"
+import {HeaderWrapper} from "./headerStyles/headerStyles"
+import Menu from "./menu"
+
+
+
+const Header = ({ siteTitle }) => {
+
+const {
+wpcontent: {menuItems},
+
+}=useStaticQuery(graphql`
+{
+  wpcontent {
+    menuItems {
+      edges {
+        node {
+          path
+          label
+        }
+      }
+    }
+  }
+}
+`
+)
+console.log(menuItems)
+return(
+  <HeaderWrapper><Menu menuItems={menuItems.edges}></Menu></HeaderWrapper>
+)
+}
+
+
+
+Header.propTypes = {
+  siteTitle: PropTypes.string,
+}
+
+Header.defaultProps = {
+  siteTitle: ``,
+}
+
+export default Header
